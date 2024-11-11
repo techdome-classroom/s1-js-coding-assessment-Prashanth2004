34f9bde241c2fc5bea6b1ee8021a13dfa04606f4
prashanth9676602530@gmail.com <youremail@example.com>
2024-11-11 13:42:54 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -4,11 +4,7 @@ const decodeTheRing = function (s, p) {
     const m = s.length;
   const n = p.length;
   const dp = Array(m + 1).fill(false).map(() => Array(n + 1).fill(false));
-
-  // Initialize base case
   dp[0][0] = true;
-
-  // Handle patterns with "*" at the start that could match an empty string
   for (let j = 1; j <= n; j++) {
     if (p[j - 1] === '*') {
       dp[0][j] = dp[0][j - 1];
