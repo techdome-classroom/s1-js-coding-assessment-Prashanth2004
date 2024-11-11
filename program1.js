929308b82da22826986327779d930cd9292a3350
prashanth9676602530@gmail.com <youremail@example.com>
2024-11-11 13:59:57 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,33 +3,33 @@ const getTotalIsles = function (grid) {
   // write your code here
    if (!mapGrid || mapGrid.length === 0) return 0;
 
-  const rowCount = mapGrid.length;
-  const colCount = mapGrid[0].length;
-  let islandCounter = 0;
-  for (let row = 0; row < rowCount; row++) {
-    for (let col = 0; col < colCount; col++) {
-      if (mapGrid[row][col] === 'L') { 
-        islandCounter++;
-        const stack = [[row, col]];
-        while (stack.length > 0) {
-          const [currRow, currCol] = stack.pop();
-          if (
-            currRow < 0 || currRow >= rowCount ||
-            currCol < 0 || currCol >= colCount ||
-            mapGrid[currRow][currCol] === 'W'
-          ) {
-            continue;
+    const rowCount = mapGrid.length;
+    const colCount = mapGrid[0].length;
+    let islandCounter = 0;
+    for (let row = 0; row < rowCount; row++) {
+      for (let col = 0; col < colCount; col++) {
+        if (mapGrid[row][col] === 'L') { 
+          islandCounter++;
+          const stack = [[row, col]];
+          while (stack.length > 0) {
+            const [currRow, currCol] = stack.pop();
+            if (
+              currRow < 0 || currRow >= rowCount ||
+              currCol < 0 || currCol >= colCount ||
+              mapGrid[currRow][currCol] === 'W'
+            ) {
+              continue;
+            }
+            mapGrid[currRow][currCol] = 'W';
+            stack.push([currRow + 1, currCol]); 
+            stack.push([currRow - 1, currCol]); 
+            stack.push([currRow, currCol + 1]); 
+            stack.push([currRow, currCol - 1]); 
           }
-          mapGrid[currRow][currCol] = 'W';
-          stack.push([currRow + 1, currCol]); 
-          stack.push([currRow - 1, currCol]); 
-          stack.push([currRow, currCol + 1]); 
-          stack.push([currRow, currCol - 1]); 
         }
       }
     }
-  }
-  return islandCounter;
+    return islandCounter;
 };
 
 module.exports = getTotalIsles;
