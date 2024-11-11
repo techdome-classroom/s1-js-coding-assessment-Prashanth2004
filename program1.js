a8eb06df6fc47cfbba2f2fd2cf77fa7a37d4678d
prashanth9676602530@gmail.com <youremail@example.com>
2024-11-11 13:30:56 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,9 +1,6 @@
 const getTotalIsles = function (grid) {
-
-
+  
   // write your code here
-
-  const getTotalIsles = function (grid) {
     if (!grid || grid.length === 0) return 0; 
     const rows = grid.length;
     const cols = grid[0].length;
